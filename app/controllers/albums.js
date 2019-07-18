260a878f7409a58cd13881c2379f0c90d2727312
Yesica Paola Nava Betancourt <yesicapaolanavabetancourt@MacBook-Pro-de-Wolox-2.local>
2019-07-18 14:56:07 -0500

    catch

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -1,9 +1,16 @@
 /* eslint-disable func-names */
 const servicesAlbums = require('../services/albums');
 
-exports.getAlbums = (req, res) => servicesAlbums.getAlbumsApi().then(albums => res.send({ albums }));
+exports.getAlbums = (req, res, next) =>
+  servicesAlbums
+    .getAlbumsApi()
+    .then(albums => res.send({ albums }))
+    .catch(() => next);
 
-exports.getAlbumsPhotos = function(req, res) {
+exports.getAlbumsPhotos = function(req, res, next) {
   const idAlbum = req.params.id;
-  servicesAlbums.getAlbumsPhotosApi(idAlbum).then(albums => res.send({ albums }));
+  servicesAlbums
+    .getAlbumsPhotosApi(idAlbum)
+    .then(albums => res.send({ albums }))
+    .catch(() => next);
 };
