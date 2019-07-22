f0558225660dc208038e97023b16dcc03a44910f
Yesica Paola Nava Betancourt <yesicapaolanavabetancourt@MacBook-Pro-de-Wolox-2.local>
2019-07-22 14:04:31 -0500

    create users

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,13 +1,16 @@
-const logger = require('../logger');
-//  User = require('../models/user');
+const logger = require('../logger'),
+  User = require('../models/user');
 
-exports.SignUp = (req, res) => {
-  // const users = new User({
-  //   email: req.body.email,
-  //   lastName: req.body.lastName,
-  //   name: req.body.name,
-  //   password: req.body.password
-  // });
+exports.SignUp = (req, res, next) => {
+  const { name, lastName, email, password } = req.body;
+
+  User.create({
+    name,
+    lastName,
+    email,
+    password
+  })
+    .then(user => res.json(user))
+    .catch(next);
   logger.info(`Hola ${req.body.email}-${req.body.name}-${req.body.password}-${req.body.lastName}`);
-  return res.send('hola');
 };
