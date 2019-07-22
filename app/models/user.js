30474b45f8eea06f80dc3e1a37ab5a1fcd42be7f
YesicaNavaB <52759744+YesicaNavaB@users.noreply.github.com>
2019-07-22 14:03:44 -0500

    User model (#4)
    * commit ApiAlbums
    
    * catch
    
    * create user model
    
    * add atribute unique
    
    * corrections
    
    * changes
    
    * return
    
    * corrections
    
    * properties are added to the model
    
    * properties are added to the model
    
    * corrections model
    
    * corrections PR
    
    * definitions property model
    
    * corrections PR

---
+++ app/models/user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'last_name'
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      paranoid: true,
+      underscored: true,
+      freezeTableName: true,
+      tableName: 'users'
+    }
+  );
+  return User;
+};
---
+++ migrations/migrations/20190718215834-create-user.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        unique: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: Sequelize.DATE,
+      updated_at: Sequelize.DATE,
+      delete_at: Sequelize.DATE
+    }),
+  down: queryInterface => queryInterface.dropTable('users')
+};
