b2925965e387281e3df7e40d62cba220060d385a
Yesica Paola Nava Betancourt <yesicapaolanavabetancourt@MacBook-Pro-de-Wolox-2.local>
2019-07-26 12:52:52 -0500

    corrections PR

--- test/users.test.js
+++ test/users.test.js
@@ -1,41 +1,47 @@
 const request = require('supertest'),
   app = require('../app');
 describe('User registration test, with their respective fields', () => {
-  it('signUp User', done => {
+  it('he user should register with all the fields correctly', done => {
     request(app)
       .post('/users')
       .send({ name: 'yesica', lastName: 'nava', email: 'yesica@wolox.co', password: 'shdfgs345*' })
       .set('Accept', 'application/json')
       .then(response => {
+        expect(response.statusCode).toBe(201);
         expect(response.text).toBe('the user was created correctly');
         done();
       });
   });
-  it('validate email', done => {
+
+  it('the user should not register because the email is incorrect', done => {
     request(app)
       .post('/users')
       .send({ name: 'yesica', lastName: 'nava', email: 'yesica.co', password: 'shdfgs345*' })
       .set('Accept', 'application/json')
       .then(response => {
+        expect(response.statusCode).toBe(400);
         expect(response.body.message[0]).toBe(
           'email yesica.co is not valid or does not belong to the wolox domain'
         );
         done();
       });
   });
-  it('validate password', done => {
+
+  it('the user should not register because the password is incorrect', done => {
     request(app)
       .post('/users')
       .send({ name: 'yesica', lastName: 'nava', email: 'yesica.co', password: 'err*' })
       .set('Accept', 'application/json')
       .then(response => {
+        expect(response.statusCode).toBe(400);
         expect(response.body.message[0]).toBe(
           'email yesica.co is not valid or does not belong to the wolox domain'
         );
         done();
       });
   });
-  it('validate required fields', done => {
+
+  it('the user should not register because no required parameter is sent', done => {
     request(app)
       .post('/users')
       .send({})
@@ -47,6 +53,7 @@ describe('User registration test, with their respective fields', () => {
           'name is required.',
           'Last name is required.'
         ];
+        expect(response.statusCode).toBe(400);
         expect(response.statusCode).toString(errors);
         done();
       });
