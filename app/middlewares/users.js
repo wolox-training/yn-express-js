be4777161ea2a33a3503adf522dc60c27c87a19f
Yesica Paola Nava Betancourt <yesicapaolanavabetancourt@MacBook-Pro-de-Wolox-2.local>
2019-07-26 17:44:11 -0500

    validations middlewares

--- app/middlewares/users.js
+++ app/middlewares/users.js
@@ -1,11 +1,39 @@
-const { validationResult } = require('express-validator'),
+const { validationResult, check } = require('express-validator'),
   error = require('../errors'),
   logger = require('../logger');
-exports.middlewareUsers = (req, res, next) => {
+
+const validateRequest = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     logger.error(JSON.stringify(errors));
-    throw error.signUpError(errors.errors);
+    next(error.validateUser(errors));
   }
   next();
 };
+
+exports.signUpMiddleware = [
+  check('name')
+    .not()
+    .isEmpty()
+    .withMessage('name is required'),
+  check('lastName')
+    .not()
+    .isEmpty()
+    .withMessage('lastName is required'),
+  check('email')
+    .not()
+    .isEmpty()
+    .withMessage('email is required')
+    .isEmail()
+    .matches(/^(([^<>\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@wolox.(co|com|com.ar)\s*$/)
+    .withMessage('email is not valid or does not belong to the wolox domain'),
+  check('password')
+    .not()
+    .isEmpty()
+    .withMessage('password is required')
+    .isAlphanumeric()
+    .withMessage('Must be only alphanumeric chars')
+    .isLength({ min: 8 })
+    .withMessage('Must be at least 8 chars long'),
+  validateRequest
+];
--- app/routes.js
+++ app/routes.js
@@ -1,39 +1,11 @@
-// const controller = require('./controllers/controller');
-const { healthCheck } = require('./controllers/healthCheck');
-const { getAlbums, getAlbumsPhotos } = require('./controllers/albums');
-const { signUp } = require('./controllers/users');
-const { middlewareUsers } = require('./middlewares/users');
-const { check } = require('express-validator');
+const { healthCheck } = require('./controllers/healthCheck'),
+  { getAlbums, getAlbumsPhotos } = require('./controllers/albums'),
+  { signUp } = require('./controllers/users'),
+  { signUpMiddleware } = require('./middlewares/users');
+
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/albums/:id?', getAlbums);
   app.get('/albums/:id/photos', getAlbumsPhotos);
-  app.post(
-    '/users',
-    [
-      check('password')
-        .not()
-        .isEmpty()
-        .withMessage('password is required')
-        .isAlphanumeric()
-        .withMessage('Must be only alphabetical chars')
-        .isLength({ min: 8 })
-        .withMessage('Must be at least 8 chars long'),
-      check('name')
-        .not()
-        .isEmpty()
-        .withMessage('name is required'),
-      check('lastName')
-        .not()
-        .isEmpty()
-        .withMessage('lastName is required'),
-      check('email')
-        .not()
-        .isEmpty()
-        .withMessage('email is required')
-        .isEmail(),
-      middlewareUsers
-    ],
-    signUp
-  );
+  app.post('/users', signUpMiddleware, signUp);
 };
