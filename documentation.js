e3d6dc6afb38980dd410cbc5032f83142bb2f9f2
YesicaNavaB <52759744+YesicaNavaB@users.noreply.github.com>
2019-07-29 09:35:12 -0500

    delete documentation.js

--- documentation.js
+++
@@ -1,9 +0,0 @@
-const dictum = require('dictum.js');
-
-// For every endpoints
-dictum.document({
-  description: 'Some description for the given endpoint',
-  endpoint: '/some/endpoint',
-  method: 'GET',
-  resource: 'My Resource'
-});
