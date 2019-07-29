50fade548ca18c4e42f3852b2de411b53df560c4
Yesica Paola Nava Betancourt <yesicapaolanavabetancourt@MacBook-Pro-de-Wolox-2.local>
2019-07-29 09:39:51 -0500

    changes var source

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -3,9 +3,8 @@ const servicesAlbums = require('../services/albums'),
   { url } = config.common.apiAlbums;
 
 exports.getAlbums = (req, res, next) => {
-  let source = url;
   const idAlbum = req.params.id;
-  source = idAlbum ? `${url}/albums/${idAlbum}` : `${url}/albums`;
+  const source = idAlbum ? `${url}/albums/${idAlbum}` : `${url}/albums`;
   return servicesAlbums
     .getAlbums(source)
     .then(albums => res.send({ albums }))
@@ -13,9 +12,8 @@ exports.getAlbums = (req, res, next) => {
 };
 
 exports.getAlbumsPhotos = (req, res, next) => {
-  let source = url;
   const idAlbum = req.params.id;
-  source = `${url}/photos?albumId=${idAlbum}`;
+  const source = `${url}/photos?albumId=${idAlbum}`;
   return servicesAlbums
     .getAlbums(source)
     .then(albumsPhotos => res.send({ albumsPhotos }))
