a4fe4b3f149d3700350d55dfc3111e97b211f3b2
Yesica Paola Nava Betancourt <yesicapaolanavabetancourt@MacBook-Pro-de-Wolox-2.local>
2019-07-30 16:14:55 -0500

    corrections PR

--- config/testing.js
+++ config/testing.js
@@ -5,7 +5,9 @@ exports.config = {
     database: {
       name: process.env.DB_NAME_TEST
     },
-
+    jwt: {
+      secret: process.env.SECRET
+    },
     session: {
       secret: 'some-super-secret'
     }
